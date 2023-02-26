70af51edcaf45f2d31ae9bb217f3660201e3858a
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-02-26 14:11:20 +0530

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const postRoutes = require("./routes/posts");
 
 //express app
@@ -16,8 +17,15 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/posts", postRoutes);
 
-app.get("/");
-//listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listeneing on port ", process.env.PORT);
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to db & listeneing on port ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
