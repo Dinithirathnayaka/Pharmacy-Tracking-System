0af99cc45119491bfab9cc566fedc7b91d287e14
Roshini109 <jbrnd21@gmail.com>
2023-04-03 17:35:41 +0530

    Added news feed functionalities

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -7,11 +7,13 @@ import React from "react";
 
 export default function Profile() {
   return (
-    <div>
-      <MainNavbar />
+    <>
+    <MainNavbar />
+    <div className="profileContainer">
       <Leftbar />
       <Feed />
       <Rightbar />
     </div>
+    </>
   );
 }
