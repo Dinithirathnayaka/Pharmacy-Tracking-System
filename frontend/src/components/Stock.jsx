20a39c53be90a861efadd453cb699336ea469294
Ayanthika Subasinghe <saasubasinghe@std.appsc.sab.ac.lk>
2023-07-21 21:47:24 +0700

    edit stock page

--- frontend/src/components/Stock.jsx
+++ frontend/src/components/Stock.jsx
@@ -1,5 +1,7 @@
 import TitleBar from "./TitleBar";
 import viewStk from "./images/analysis.png";
+import "../Styles/Stock.css";
+
 
 const Stock = () => {
   return (
@@ -10,7 +12,73 @@ const Stock = () => {
         description="View available medicine"
       />
       <div className="container">
-        <h1>Stock Details</h1>
+
+        <div>
+          <hr/>
+          <button  className="addbutton" style={{marginBottom:"10px"}}>Add medicine</button>
+        </div>
+
+        <table>
+          <tr>
+            <th>Batch</th>
+            <th>Name</th>
+            <th>Company</th>
+            <th>Quantity</th>
+            <th>Ex:Date</th>
+            <th>Type</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>
+              <img src="/assets/images/edit.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>
+              <img src="/assets/images/bin.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>
+              
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>
+            <img src="/assets/images/edit.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>
+              <img src="/assets/images/bin.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>
+            </td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>
+            <img src="/assets/images/edit.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>
+              <img src="/assets/images/bin.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>    
+            </td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>Batch No</td>
+            <td>
+            <img src="/assets/images/edit.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>
+              <img src="/assets/images/bin.png" style={{height: "20px",width:"20px",marginRight: "5px"}}/>
+            </td>
+          </tr>
+        </table>
+  
       </div>
     </>
   );
