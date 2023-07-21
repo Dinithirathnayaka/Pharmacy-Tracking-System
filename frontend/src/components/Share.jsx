3b6b24f4a7cc1e189849a04a480269dc2f1bc809
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-07-22 00:11:23 +0530

    modified edit profile image link

--- frontend/src/components/Share.jsx
+++ frontend/src/components/Share.jsx
@@ -1,21 +1,28 @@
-import { PermMedia, LocalOffer, AddReaction, LocationOn } from "@mui/icons-material";
-import {useState} from "react"
+import {
+  PermMedia,
+  LocalOffer,
+  AddReaction,
+  LocationOn,
+} from "@mui/icons-material";
+import { useState } from "react";
 import Createpost from "./Createpost";
+import { NavLink } from "react-router-dom";
 import "../Styles/Share.css";
 
 export default function Share() {
-
-  const [openModal,setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <div className="share">
       <div className="shareWrapper">
         <div className="shareTop">
-          <img
-            className="shareProfileImg"
-            src="/assets/images/person/1.jpg"
-            alt="person1"
-          />
+          <NavLink to="editprofile">
+            <img
+              className="shareProfileImg"
+              src="/assets/images/person/1.jpg"
+              alt="person1"
+            />
+          </NavLink>
           <input placeholder="What's in your mind?" className="shareInput" />
         </div>
         <hr className="shareHr" />
@@ -37,12 +44,16 @@ export default function Share() {
               <LocationOn htmlColor="red" className="shareIcon" />
               <span className="shareIconText">Location</span>
             </div>
-            
           </div>
-          <button className="shareButton" onClick={() => {
-                    setOpenModal(true);
-                }}>Share</button>
-                {openModal && <Createpost closeModal={setOpenModal} />}
+          <button
+            className="shareButton"
+            onClick={() => {
+              setOpenModal(true);
+            }}
+          >
+            Share
+          </button>
+          {openModal && <Createpost closeModal={setOpenModal} />}
         </div>
       </div>
     </div>
