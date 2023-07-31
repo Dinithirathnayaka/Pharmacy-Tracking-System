224fd4120fd5b11f4ad779cfd596d618b83bbd00
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-07-31 19:50:49 +0530

    Modified footer section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,7 +8,6 @@ import {
   FaMapMarkerAlt,
   FaPhone,
 } from "react-icons/fa";
-import { MarkunreadMailbox } from "@mui/icons-material";
 
 const Hero = () => {
   return (
@@ -66,7 +65,7 @@ const Hero = () => {
             <div className="col-md-4 col-xs-12 p-2">
               {" "}
               <h5>ABOUT US</h5>
-              <div className="mb-3">
+              <div className="mb-3" style={{ fontWeight: "600" }}>
                 We help you to ensure that you have access to the medications
                 you require to lead a healthy and fulfilling life.If you join
                 hand with us you can get amazing services from us.
@@ -81,29 +80,39 @@ const Hero = () => {
             <div className="col-md-3 col-xs-12 p-2">
               {" "}
               <h5>SERVICES</h5>
-              <div className="mb-2 text-bold">Search Medicine</div>
-              <div className="mb-2">Find Pharmacy</div>
-              <div className="mb-2">Meet Doctor</div>
+              <div className="mb-2 text-bold" style={{ fontWeight: "600" }}>
+                Search Medicine
+              </div>
+              <div className="mb-2" style={{ fontWeight: "600" }}>
+                Find Pharmacy
+              </div>
+              <div className="mb-2" style={{ fontWeight: "600" }}>
+                Meet Doctor
+              </div>
             </div>
 
             <div className="col-md-3 col-xs-12 p-2">
               <h5>HAVE A QUESTIONS?</h5>
               <div className="mb-2">
                 <FaMapMarkerAlt className="contacticon" />
-                <span>203 Fake St. Mountain View, USA</span>
+                <span style={{ fontWeight: "600" }}>
+                  203 Fake St. Mountain View, USA
+                </span>
               </div>
               <div className="mb-2">
                 <FaPhone className="contacticon" />
-                <span>+2 392 3929 210</span>
+                <span style={{ fontWeight: "600" }}>+2 392 3929 210</span>
               </div>
               <div className="mb-2">
                 <MdEmail className="contacticon" />
-                <span>pts@gmail.com</span>
+                <span style={{ fontWeight: "600" }}>pts@gmail.com</span>
               </div>
             </div>
           </div>
         </div>
-        <p className="text-center">©2023 All Rights Reserved</p>
+        <p className="text-center" style={{ fontWeight: "600" }}>
+          ©2023 All Rights Reserved
+        </p>
       </div>
     </>
   );
