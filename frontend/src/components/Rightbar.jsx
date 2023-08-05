15f304f2546d9387d346792cac1f792c2903e95f
Viraj Tharuka Ranathunga <virajtharuka.fb@gmail.com>
2023-08-05 12:41:57 +0530

    Some CSS updates

--- frontend/src/components/Rightbar.jsx
+++ frontend/src/components/Rightbar.jsx
@@ -4,20 +4,23 @@ export default function Rightbar() {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
-
         {/* <div className="birthdayContainer">
           <img src="/assets/images/birthday/4.png" alt="" className="birthdayImg" />
           <span className="birthdayText"><b>Pola Foster</b> and <b>3 others</b> have birthdays today!</span>
         </div> */}
 
-        <img src="/assets/ad1.png" alt="" className="rightbarAd" />
+        {/* <img src="/assets/ad1.png" alt="" className="rightbarAd" /> */}
 
         <h4 className="rightbarTitle">Online Friends</h4>
 
         <ul className="onlineFriendList">
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="/assets/images/person/1.jpg" alt="" className="rightbarProfileImg" />
+              <img
+                src="/assets/images/person/1.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
               <span className="rightbarOnline"></span>
             </div>
 
@@ -26,7 +29,11 @@ export default function Rightbar() {
 
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="/assets/images/person/2.jpg" alt="" className="rightbarProfileImg" />
+              <img
+                src="/assets/images/person/2.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
               <span className="rightbarOnline"></span>
             </div>
 
@@ -35,7 +42,11 @@ export default function Rightbar() {
 
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="/assets/images/person/3.jpg" alt="" className="rightbarProfileImg" />
+              <img
+                src="/assets/images/person/3.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
               <span className="rightbarOnline"></span>
             </div>
 
@@ -44,17 +55,18 @@ export default function Rightbar() {
 
           <li className="rightbarFriend">
             <div className="rightbarProfileImgContainer">
-              <img src="/assets/images/person/4.jpg" alt="" className="rightbarProfileImg" />
+              <img
+                src="/assets/images/person/4.jpg"
+                alt=""
+                className="rightbarProfileImg"
+              />
               <span className="rightbarOnline"></span>
             </div>
 
             <span className="rightbarUserName"> Joana Christiana</span>
           </li>
-
-         
         </ul>
-
       </div>
     </div>
-  )
+  );
 }
