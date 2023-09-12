56aea2364a9ae78b1bdded9418833d7c454e16a5
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-09-12 14:03:13 +0530

    modified doctor registration

--- frontend/src/hooks/useRegister.js
+++ frontend/src/hooks/useRegister.js
@@ -6,29 +6,30 @@ export const useRegister = () => {
   const [isLoading, setIsLoading] = useState(null);
   const { dispatch } = useAuthContext();
 
-  const signup = async (username, email, password) => {
+  const signup = async (username, email, password, role, roleData) => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("/api/user/signup", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, email, password }),
-    });
-    const json = await response.json();
+    try {
+      const response = await fetch("/api/user/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username,
+          email,
+          password,
+          role,
+          roleData: roleData,
+        }),
+      });
 
-    if (!response.ok) {
-      setIsLoading(false);
-      setError(json.error);
-    }
-    if (response.ok) {
-      //save the user to local storage
-      localStorage.setItem("user", JSON.stringify(json));
-
-      //update the auth context
-      dispatch({ type: "LOGIN", payload: json });
-
-      setIsLoading(false);
+      if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.error);
+      }
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
     }
   };
 
